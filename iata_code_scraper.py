1584df7d7ff99db127df450d9007f75c8fdec0db
FarazNaseem <faraz.n019@outlook.com>
2020-09-27 18:30:17 -0400

    Instead of scraping a website for the iata codes of airports, we will use an API known as pycountries, which will get the country codes entered, and then send that information to another api, which will convert the information into the required format needed to run the Skyscraper API.

--- iata_code_scraper.py
+++ iata_code_scraper.py
@@ -1,8 +1,10 @@
-from bs4 import BeautifulSoup
-import requests
+import pycountry
 
-url = "https://www.nationsonline.org/oneworld/IATA_Codes/airport_code_list.htm"
-response = requests.get(url)
-data = response.text
-soup = BeautifulSoup(data, 'html.parser')
-table = soup.find("table")
+number_countries = len(pycountry.countries)
+
+country_lookup = "United Kingdom"
+
+for country in range(number_countries):
+    if list(pycountry.countries)[country].name == country_lookup:
+        print("Found United Kingdom's code, it is: " + list(pycountry.countries)[country].alpha_2)
+
