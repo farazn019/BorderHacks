aa2e710aa58104d865dbf299df148909c87b77ed
FarazNaseem <faraz.n019@outlook.com>
2020-09-27 19:04:39 -0400

    I integreated the country_code function from the iata_code_scraper.py file with the flaskmain.py file. Now it will determine the country code for the outbound country.

--- flaskmain.py
+++ flaskmain.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect # Imports the flask framework.
 from flask_bootstrap import Bootstrap
 from . forms import TicketForm
+from . iata_code_scraper import country_code
+import re
 
 app = Flask(__name__)
 app.secret_key = "SECRET"
@@ -9,19 +11,28 @@ Bootstrap(app)
 @app.route('/', methods=("GET", "POST"))
 @app.route('/home', methods=("GET", "POST"))
 def homepage():
-    ticketform = TicketForm()
+
+    ticket_form = TicketForm()
+
     if request.method == "POST":
         departure_location = request.form['current_city']
+        departure_city = re.split(', ', departure_location)[0]
+        departure_country = re.split(", ", departure_location)[1]
+        country_code(departure_country)
+
         destination = request.form['destination_location']
+        destination_city = re.split(', ', destination)[0]
+        destination_country = re.split(', ', destination)[1]
+
         departure_date = request.form['leave']
         return_date = request.form['returning']
 
-        return redirect(url_for('pricespage'))
-    return render_template('webpage.html', form=ticketform)
+        return redirect(url_for('prices_page'))
+    return render_template('webpage.html', form=ticket_form)
 
 
 @app.route('/prices', methods=("GET", "POST"))
-def pricespage():
+def prices_page():
     return render_template('pricespage.html')
 
 if __name__ == 'main':
--- iata_code_scraper.py
+++ iata_code_scraper.py
@@ -1,10 +1,12 @@
 import pycountry
 
-number_countries = len(pycountry.countries)
 
-country_lookup = "United Kingdom"
+def country_code(country_name):
+    number_countries = len(pycountry.countries)
 
-for country in range(number_countries):
-    if list(pycountry.countries)[country].name == country_lookup:
-        print("Found United Kingdom's code, it is: " + list(pycountry.countries)[country].alpha_2)
+    # country_lookup = "United Kingdom"
+
+    for country in range(number_countries):
+        if list(pycountry.countries)[country].name == country_name:
+            print("Found the country code for " + country_name + ". It is, "+list(pycountry.countries)[country].alpha_2)
 
